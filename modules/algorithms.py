6343d450409e6cc8893379a7cac3a3024c02bdad
Brendan Oelofse <Oelofse30@gmail.com>
2017-04-17 10:44:53 +0200

    Added algorithm for the inverse of modulo from:https://rosettacode.org/wiki/Modular_inverse#Python

--- modules/algorithms.py
+++ modules/algorithms.py
@@ -19,3 +19,19 @@ def euclidean(x, y):
         y = z
     
     return y
+
+class ModInverse:
+    def extended_gcd(self, aa, bb):
+        lastremainder, remainder = abs(aa), abs(bb)
+        x, lastx, y, lasty = 0, 1, 1, 0
+        while remainder:
+            lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+            x, lastx = lastx - quotient * x, x
+            y, lasty = lasty - quotient * y, y
+        return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+
+     def modinv(self, a, m):
+        g, x, y = self.extended_gcd(a, m)
+        if g != 1:
+            raise ValueError
+        return x % m
