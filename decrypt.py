008d276ada5f0d30e5b60a425cedcc16fba97b93
Brendan Oelofse <Oelofse30@gmail.com>
2017-04-17 15:00:43 +0200

    Used algorithm to retrieve letter positions in alphabet

--- decrypt.py
+++ decrypt.py
@@ -1,2 +1,9 @@
+from modules import algorithms
+
 c_text = input("Enter ciphertext: ")
 pad = input("Enter pad: ")
+
+c_text_num = algorithms.alph_pos(list(c_text))
+pad_num = algorithms.alph_pos(list(pad))
+
+
