bbb6111da1796a5a6a5cc41c009df67f3a6b3a75
Brendan Oelofse <Oelofse30@gmail.com>
2017-04-22 13:12:04 +0200

    Added devryption logic

--- decrypt.py
+++ decrypt.py
@@ -1,9 +1,17 @@
 from modules import algorithms
 
+
 c_text = input("Enter ciphertext: ")
 pad = input("Enter pad: ")
 
 c_text_num = algorithms.alph_pos(list(c_text))
 pad_num = algorithms.alph_pos(list(pad))
 
+p_text_num = []
+for p in range(len(c_text_num)):
+    y = algorithms.modinverse((c_text_num[p] - pad_num[p]), 26)
+    p_text_num.append(y)
 
+p_text = algorithms.num_alph(p_text_num)
+
+print("".join(p_text))
